55073780b62f4b65c34e55ab3084bd68707d6742
Anatolii Khramchenko <71408259+xtor007@users.noreply.github.com>
2023-03-30 22:14:13 +0300

    Initial Commit

---
+++ Cleaner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cleaner
+//
+//  Created by Anatoliy Khramchenko on 30.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
