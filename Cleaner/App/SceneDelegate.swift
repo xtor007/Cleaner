8f048b38398bbecbe4b3fbf34fbc7a39be5ce0b0
Anatolii Khramchenko <71408259+xtor007@users.noreply.github.com>
2023-03-31 22:59:06 +0300

    Create background

--- Cleaner/App/SceneDelegate.swift
+++ Cleaner/App/SceneDelegate.swift
@@ -14,7 +14,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        let mainVC = MainViewController(viewModel: MainViewModel())
+        // let mainVC = MainViewController(viewModel: MainViewModel())
+        let mainVC = CleanerViewController(viewModel: CleanerViewModel())
         let navigationController = UINavigationController(rootViewController: mainVC)
         window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
