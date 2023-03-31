4e21db22739b4e71cbc577fb0af17bf124f7d644
Anatolii Khramchenko <71408259+xtor007@users.noreply.github.com>
2023-03-31 21:47:13 +0300

    Add SwiftLint and SwiftGen

--- Cleaner/Flow/Main/View/MainViewController.swift
+++ Cleaner/Flow/Main/View/MainViewController.swift
@@ -8,18 +8,18 @@
 import UIKit
 
 class MainViewController: UIViewController {
-    
+
     private let viewModel: MainViewModel
 
     init(viewModel: MainViewModel) {
         self.viewModel = viewModel
         super.init(nibName: nil, bundle: nil)
     }
-    
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }
